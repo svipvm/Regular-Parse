f3926de79325d5e3de9ecc0685ff6bd0ec65c7e5
svipvm <svipvm@yeah.net>
2023-02-25 08:31:54 +0800

    add content match

--- re_parse.py
+++ re_parse.py
@@ -1,15 +1,19 @@
 import argparse, json, os, re
 
-def read_data(filename, config):
-    with open(filename, "r") as f:
-        text_data = "".join(f.readlines())
-        text_data = text_data.replace("\n", " ")
-        text_data = text_data.replace("\t", " ")
+def read_data(config, filename=None, content=None):
+    if content:
+        text_data = content
+    else:
+        with open(filename, "r") as f:
+            text_data = "".join(f.readlines())
+            text_data = text_data.replace("\n", " ")
+            text_data = text_data.replace("\t", " ")
     with open(config, "r") as f:
         json_data = json.load(f)['nodelist']
     return text_data, json_data
 
 def to_parse(text_data, json_data):
+    # print(text_data, json_data)
     result = {}
     for idx, node in enumerate(json_data):
         if node['task'] not in result:
@@ -40,11 +44,23 @@ if __name__ == '__main__':
     arg_parse = argparse.ArgumentParser()
     arg_parse.add_argument("-f", "--filename", help="input file path")
     arg_parse.add_argument("-c", "--config", help="input config path")
+    arg_parse.add_argument("-t", "--text", help="input content")
     args = arg_parse.parse_args()
 
     result = ""
-    if os.path.isfile(args.filename) and os.path.isfile(args.config):
-        text_data, json_data = read_data(args.filename, args.config)
+    if args.config:
+        if not os.path.isfile(args.config):
+            raise Exception("Input correct configure file, please.")
+        # print(args.filename)
+        if args.filename:
+            if not os.path.isfile(args.filename):
+                raise Exception("Input correct content file, please.")
+            text_data, json_data = read_data(args.config, filename=args.filename)
+        elif args.text:
+            text_data, json_data = read_data(args.config, content=args.text)
+        else:
+            text_data, json_data = None, None
+
         result = json.dumps(to_parse(text_data, json_data))
     
     print(result)
